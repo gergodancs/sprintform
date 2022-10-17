c3faab393cc5c1660250a24a84aab4fa30b30a97
Gergely Dancs <dancs.gergely@flexoon.hu>
2022-10-17 13:01:19 +0200

    Fix Ui

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,6 @@
 import { useTranslation } from "react-i18next";
 import "./styles/header.scss";
 import { useEffect } from "react";
-import ChangeLangBtn from "./ChangeLangBtn";
 
 const Header = () => {
   const { t } = useTranslation();
--- src/components/ListItemFilter.tsx
+++ src/components/ListItemFilter.tsx
@@ -2,7 +2,6 @@ import "./styles/listItemFilter.scss";
 import { useContext } from "react";
 import SelectCategory from "./SelectCategory";
 import Button from "./Button";
-import ChangeLangBtn from "./ChangeLangBtn";
 import StoreCtx from "../store-context/store";
 import { useTranslation } from "react-i18next";
 
@@ -23,7 +22,7 @@ const ListItemFilter = () => {
   return (
     <div className="filter-container">
       <SelectCategory />
-      <Button text={t("show")} onClick={showSpends} />
+      <Button text={t(showList ? "hide" : "show")} onClick={showSpends} />
     </div>
   );
 };
